8efc9d17502cd3bef6d8261693431d079a0d5d84
Nicolas BRAILLON <nicolas.braillonpro@gmail.com>
2023-03-27 12:09:05 +0200

    Ajout bouton supprimer la recherche par catégories

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -8,27 +8,36 @@ const Filter = ({
 }) => {
   const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
   return (
-    <ul className="radio-container">
-      <input
-        type="range"
-        min="1"
-        max="250"
-        defaultValue={rangeValue}
-        onChange={(e) => setRangeValue(e.target.value)}
-      />
+    <div>
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
 
-      {radios.map((continent) => (
-        <li key={continent}>
-          <input
-            type="radio"
-            id={continent}
-            name="continentRadio"
-            onChange={(e) => setSelectedContinent(e.target.id)}
-          />
-          <label htmlFor={continent}>{continent}</label>
-        </li>
-      ))}
-    </ul>
+        {radios.map((continent) => (
+          <li key={continent}>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              onChange={(e) => setSelectedContinent(e.target.id)}
+              checked={continent === selectedContinent}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
+      </ul>
+
+      {selectedContinent && (
+        <button onClick={() => setSelectedContinent("")}>
+          Annuler la recherche
+        </button>
+      )}
+    </div>
   );
 };
 
