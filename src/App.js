aab8068c48eb323655605c3aefdbfd0de5b1ce2e
Nicolas BRAILLON <nicolas.braillonpro@gmail.com>
2023-03-27 09:36:20 +0200

    Création composant de base + création des routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './pages/About'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
